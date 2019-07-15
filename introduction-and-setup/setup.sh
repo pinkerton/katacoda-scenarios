363a7913ec953e3258e6147446d44218904ddcdc
Stephen Firrincieli <stephen.firrincieli@datadoghq.com>
2019-07-15 16:47:16 -0400

    Remove node install from setup.sh

--- introduction-and-setup/setup.sh
+++ introduction-and-setup/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set +x
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -;
-sudo apt-get install -y nodejs;
+# curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -;
+# sudo apt-get install -y nodejs;
