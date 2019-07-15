f90f9111274c488f98b763f2e9a42f31aa2208d0
Stephen Firrincieli <stephen.firrincieli@datadoghq.com>
2019-07-15 15:59:38 -0400

    Don't create workshop dir

--- introduction-and-setup/setup.sh
+++ introduction-and-setup/setup.sh
@@ -2,6 +2,5 @@
 set -ex
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -;
 sudo apt-get install -y nodejs;
-mkdir /root/workshop;
 touch /root/workshop/serverless.yml;
 touch /root/workshop/create-image-upload-url.js;
