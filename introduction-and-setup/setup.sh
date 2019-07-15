2a3ecf4f5c3c2fc5ef9cbd8fe1c128cc679bddd7
Stephen Firrincieli <stephen.firrincieli@datadoghq.com>
2019-07-15 16:08:37 -0400

    Make assets in index.json

--- introduction-and-setup/setup.sh
+++ introduction-and-setup/setup.sh
@@ -2,5 +2,3 @@
 set +x
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -;
 sudo apt-get install -y nodejs;
-touch /root/workshop/serverless.yml;
-touch /root/workshop/create-image-upload-url.js;
