385c2b521b68c9b90b0c4badf153089a38dc7586
Stephen Firrincieli <stephen.firrincieli@datadoghq.com>
2019-07-15 15:20:26 -0400

    Initialize files in setup script

--- introduction-and-setup/setup-node.sh
+++ introduction-and-setup/setup-node.sh
@@ -1,2 +1,4 @@
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -;
 sudo apt-get install -y nodejs;
+touch /root/workshop/serverless.yml;
+touch /root/workshop/create-image-upload-url.js;
