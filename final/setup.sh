07aac6fa80b79f0459ef8178154302844ecbc4af
Stephen Firrincieli <stephen.firrincieli@datadoghq.com>
2019-07-15 20:39:58 -0400

    Write creds to aws location

--- final/setup.sh
+++ final/setup.sh
@@ -2,5 +2,8 @@
 # source /root/setupaws.sh
 ENDPOINT="https://dd.pinkerton.io/4d8bd97f03109ac1ffd4f20894ed216d86476ec97220a3ae5a8a5b3078892139"
 RESULT=$( curl -s -d "" "$ENDPOINT" )
-echo "export AWS_ACCESS_KEY_ID=$( echo $RESULT | jq -r .key )" >> ~/.bashrc
-echo "export AWS_SECRET_ACCESS_KEY=$( echo $RESULT | jq -r .secret )" >> ~/.bashrc
+mkdir ~/.aws
+touch ~/.aws/credentials
+echo "[default]" >> ~/.aws/credentials
+echo "aws_access_key_id=$( echo $RESULT | jq -r .key )" >> ~/.aws/credentials
+echo "aws_secret_access_key=$( echo $RESULT | jq -r .secret )" >> ~/.aws/credentials
