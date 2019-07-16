6ddb43b811b7338b52407f252437d752c8ef9498
Stephen Firrincieli <stephen.firrincieli@datadoghq.com>
2019-07-15 20:11:19 -0400

    Try to add courseData

--- final/setup.sh
+++ final/setup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-/root/setupaws.sh
+# source /root/setupaws.sh
+ENDPOINT="https://dd.pinkerton.io/4d8bd97f03109ac1ffd4f20894ed216d86476ec97220a3ae5a8a5b3078892139"
+RESULT=$( curl -s -d "" "$ENDPOINT" )
+echo "AWS_ACCESS_KEY_ID=$( echo $RESULT | jq -r .key )" >> ~/.bashrc
+echo "AWS_SECRET_ACCESS_KEY=$( echo $RESULT | jq -r .secret )" >> ~/.bashrc
